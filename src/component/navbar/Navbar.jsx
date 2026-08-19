9a4cc9f112d71542ba5c04bed2cd272a3afc9cae
https://github.com/Hassaan-Hub <hassaanali994@gmail.com>
2026-08-19 13:00:45 +0500

    Update Navbar component layout and styles; add new icons

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import Logo from '../assets/logo.png'
+import Logo from '../../assets/logo.png'
+import Dotdot from '../../assets/icon.png'
 import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div className='flex text-lg bg-black text-white justify-between items-center px-10 py-3'>
+    <div className='flex text-lg bg-black text-white font-semibold justify-between items-center px-10 py-3'>
       <div>
         <img src={Logo} alt="image" />
       </div>
       <div>
-        {/* <RiSearchLine /> */}
          <input className='border-2 px-10 py-2 rounded' type="text" placeholder='Search' />
       </div>
-      <div className='flex gap-5 items-center'>
+      <div className='flex gap-7 items-center'>
+        <img src={Dotdot} alt="" />
         <button>Login</button>
-        <button className='signup px-3 rounded'>Sign up</button>
+        <button className='signup px-3 text-black'>Sign up</button>
       </div>
     </div>
   )
