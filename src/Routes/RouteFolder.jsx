95e83211f920d986044ac77880e998076f21e45d
https://github.com/Hassaan-Hub <hassaanali994@gmail.com>
2026-08-29 18:55:37 +0500

    Add react-router-dom for routing and create RouteFolder component

---
+++ src/Routes/RouteFolder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RouteFolder = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default RouteFolder
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import "remixicon/fonts/remixicon.css";
 import CardsDataContextProvider from './Context/CardsDataContextProvider.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CardsDataContextProvider>
-      <App />
-    </CardsDataContextProvider>
+    <BrowserRouter>
+      <CardsDataContextProvider>
+        <App />
+      </CardsDataContextProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
