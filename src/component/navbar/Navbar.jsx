28402e62bb121ff50eb5ef2122d9ddb31a44a447
https://github.com/Hassaan-Hub <hassaanali994@gmail.com>
2026-09-02 16:54:06 +0500

    Implement search functionality in Navbar and enhance Search component layout

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../../assets/logo.png'
 import Dotdot from '../../assets/icon.png'
 import searchIcon from '../../assets/search.png'
 import './navbar.css'
 import { useNavigate } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({ children }) => {
   const navigate = useNavigate();
+
+  const [search, setSearch] = useState('')
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+    navigate('/search', { state: { searchText: e.target.value } });
+  }
+
+
   return (
     <div className='mainNav flex text-lg bg-black text-white font-semibold justify-between items-center px-10 py-1'>
       <div>
         <img src={Logo} alt="image" />
       </div>
-      <div className='inputDiv' onClick={()=> navigate('/search')}>
+      <div className='inputDiv'>
         <img src={searchIcon} alt="Search" />
-        <input className='border-none outline-none' type="text" placeholder='Search' />
+        <input
+          className='border-none outline-none'
+          type="text"
+          placeholder='Search'
+          value={search}
+          onChange={handleSearch}
+        />
       </div>
       <div className='flex gap-7 items-center'>
         <img src={Dotdot} alt="" />
--- src/component/searchFol/Search.jsx
+++ src/component/searchFol/Search.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { CardsDataContext } from '../../Context/CardsDataContextProvider';
+import React, { useContext } from 'react'
+import './search.css'
+import { useLocation } from 'react-router-dom';
 
-const Search = () => {
+
+const Search = ({ children }) => {
+
+  const videos = useContext(CardsDataContext);
+
+  const location = useLocation();
+
+  const searchText = location.state?.searchText || '';
+
+  const filteredVideos = videos.filter((val) =>
+    val.title.toLowerCase().includes(searchText.toLowerCase())
+  );
+
   return (
-    <div>
-        <h1 className='text-amber-200 text-5xl'>search page</h1>
+    <div className='srchMain'>
+
+      {filteredVideos.map((val, idx) => {
+        return (
+          <div className='flex' key={idx}>
+            <img width={220} height={133.093017578125} src={val.thumbnail} alt="image" />
+            <div>
+              <p>{val.title}</p>
+              <p>{val.views} • {val.time}</p>
+              <div className='flex'>
+                <img className='srchprofile' src={val.profile} alt="pr" />
+                <p>{val.author}</p>
+              </div>
+            </div>
+          </div>
+        )
+      })}
+
     </div>
   )
 }
